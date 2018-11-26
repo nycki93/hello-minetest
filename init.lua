ad3003918086a5f01e1e6f176567200ff25cca1e
nupanick <nupanick@gmail.com>
2018-11-25 22:26:09 -0800

    Register the other blocks.

--- init.lua
+++ init.lua
@@ -3,18 +3,15 @@
 -- To practice with the minetest API
 
 letters = {"H", "E", "L", "O", "W", "R", "D"}
+for _, letter in ipairs(letters) do
+    lower = letter:lower()
+    face_texture = "hello_" .. lower .. ".png"
+    block_name = "hello:" .. lower .. "_block"
+    block_desc = letter .. " Block"
 
-
-minetest.register_node("hello:h_block", {
-    description = "H Block",
-    tiles = {"hello_blank.png^hello_h.png"},
-    groups = {choppy=3, dig_immediate=2}
-
-})
-
-minetest.register_node("hello:O_block", {
-    description = "O Block",
-    tiles = {"hello_blank.png^hello_o.png"},
-    groups = {choppy=3, dig_immediate=2}
-
-})
+    minetest.register_node(block_name, {
+        description = block_desc,
+        tiles = {"hello_blank.png^" .. face_texture},
+        groups = {choppy=3, dig_immediate=2}
+    })
+end
